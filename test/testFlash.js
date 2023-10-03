4c6d035b6ac1756421eac4741905f32345fb2ae8
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-10-03 15:56:44 +0530

    adding testing file

---
+++ test/testFlash.js
@@ -0,0 +1,36 @@
+const {expect, assert} = require("chai");
+const {ethers} = require("hardhat");
+const {fundContract} = require("../utils/utilities");
+
+const {
+    abi,
+} = require("../artifacts/contracts/interfaces/IERC20.sol/IERC20.json");
+
+const provider = waffle.provider;
+
+describe('Flashloan Contract', () => { 
+    let FLASHLOAN,
+    BORROW_AMOUNT,
+    FUND_AMOUNT,
+    initialFundingHuman,
+    txArbitrage;
+
+    const DECIMALS = 18;
+
+    const BUSD_WHALE = "0xf977814e90da44bfa03b6295a0616a897441acec";
+    const BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+    const CAKE = "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82";
+    const CROX = "0x2c094F5A7D1146BB93850f629501eB749f6Ed491";
+
+    const busdInstance = new ethers.Contract(BUSD, abi, provider)
+
+    beforeEach(async () => {
+        const whale_balance = await provider.getBalance(BUSD_WHALE);
+        console.log(whale_balance);
+    })
+    
+    it("testing",()=> {
+        
+    })
+
+})
